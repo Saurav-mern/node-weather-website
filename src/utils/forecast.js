fdac7ccf9af0c4eada883a57a4ca56ca36f6203c
Saurav <sauravsingh1@outlook.in>
2020-09-13 12:03:47 +0530

    Add about text and some more information from weatherstack API is shown now

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,9 +11,12 @@ const forecast = (latitude, longitude, callback) => {
     } else if (response.body.error) {
         callback("Unable to find location", undefined);
     } else {
-        callback(undefined, 
-        `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out but it feels like ${response.body.current.feelslike}`
-    );
+        callback(
+          undefined,
+          `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees out and it feels like ${response.body.current.feelslike}. 
+          The local time is ${response.body.location.localtime}.
+          Humidity is ${response.body.current.humidity}.  Wind speed is ${response.body.current.wind_speed} km/h and wind direction is ${response.body.current.wind_degree} ${response.body.current.wind_dir}. `
+        );
     }
   });
 }
